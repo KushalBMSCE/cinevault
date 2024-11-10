1172f11062e24af4399af5c956164e2fe14be2b0
KushalBMSCE <kushalnaidu.cs24@bmsce.ac.in>
2024-11-10 16:36:47 +0530

    Add files via upload

---
+++ New folder (2)/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CineVault from './components/CineVault';
+
+function App() {
+  return (
+    <div>
+      <CineVault />
+    </div>
+  );
+}
+
+export default App;
